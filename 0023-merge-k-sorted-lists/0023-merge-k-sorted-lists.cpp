fed58946cc97ab6930c21e853350293c9e8e7e3f
vtu27398-CSE <vtu27398@veltech.edu.in>
2026-04-02 10:02:38 +0530

    Time: 2 ms (69.31%), Space: 18.3 MB (81.65%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;
+
+        for (ListNode* node : lists) {
+            if (node != nullptr) {
+                pq.push(node);
+            }
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* node = pq.top();
+            pq.pop();
+
+            tail->next = node;
+            tail = tail->next;
+
+            if (node->next != nullptr) {
+                pq.push(node->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
