2cbff7136e5f2db9b95ef4f7c1f818fa2c50b906
vtu27398-CSE <vtu27398@veltech.edu.in>
2026-04-02 09:56:19 +0530

    Time: 96 ms (19.36%), Space: 93.8 MB (66.74%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> window;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (window.count(nums[i])) {
+                return true;
+            }
+            
+            window.insert(nums[i]);
+            
+            // Maintain window size of at most k
+            if (window.size() > k) {
+                window.erase(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+};
